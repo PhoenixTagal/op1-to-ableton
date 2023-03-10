ee7855b53f37f6d59412ffaae321d766eef739ad
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-09 21:29:00 -0800

    troubleshoot
    worked through ndarray object through program flow to better understand parameters.
    
    print statements on this page have a useful template for reading properties of a wave file.
    
    will create a new .py file with conversion that is succinct and can easily be passed into a GUI

---
+++ wav_diagnostic.py
@@ -0,0 +1,32 @@
+import soundfile as sf
+import numpy as np
+
+unsupported_file = 'track_2.wav'
+supported_file = 'supported_track_2.wav'
+
+# passing 'float32' as argument for dtype parameter converts from original file datatype
+data, samplerate = sf.read(unsupported_file, dtype='float32')
+
+print('data is list of numpy.ndarrays \n')
+print('each element of list holds 2 numpy.float64 objects')
+print('data:', data, '\n')
+print('length of data:', len(data))
+print('object type of data variable:', type(data))
+print('sample rate:', samplerate)
+
+
+# looking at array dimensions of data
+data_array_info = np.asarray(data)
+print('tuple specifiying size of each dimension of the var data array:', data_array_info.shape)
+
+print(data[1])
+
+for i in data[1]:
+    print(type(i))
+    print(type(float(i)))
+
+# writing new file with new data type established in sf.read() args
+sf.write(data=data,
+         samplerate=samplerate,
+         endian='LITTLE',
+         file=supported_file)
